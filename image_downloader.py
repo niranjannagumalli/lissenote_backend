1f59d100a90bb2a10bca133702e03f39339ee5fe
niranjannagumalli <87712842+niranjannagumalli@users.noreply.github.com>
2022-08-11 09:07:01 +0530

    add docstring and clean code

--- image_downloader.py
+++ image_downloader.py
@@ -1,11 +1,23 @@
 import urllib.request
 from image_link_fetcher import *
 def download_images(images,file_path='./images/'):
+     """
+    Give input as a dictionary with keywords and their respective url's,
+    you get the output dictionary with keywords and their relative location as corresponding values.    
+    The image will be downloaded in the /images directory within the current working directory
+    
+    Parameters:
+    arg1 (dictionary): Dictionary with image title as key and image URL as the value associated with the key
+    
+    Returns:
+    dictionary : Dictionary with image title as key and image location on the local device the value associated with the key
+  
+    """
     image_loc = {}
     for key in images:
         full_path = file_path + key + '.jpg'
         image_loc[key] = full_path
         urllib.request.urlretrieve(images[key], full_path)
     return image_loc
-#give input as a dictionary with keywords and their respective url's, you get the output dictionary with keywords and their relative location as corresponding values
-download_images(get_img_links(["cats","tabletennis","iiit kottayam"]))
+#Here is an example, Uncomment it to test it.
+#download_images(get_img_links(["Transistors","capacitors","moores law"]))
